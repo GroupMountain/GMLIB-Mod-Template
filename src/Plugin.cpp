dc0428ea9b44c3ea6a0fedd7c49eb77e92221b6b
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2024-01-08 21:13:10 +0800

    feat: update SDK

---
+++ src/Plugin.cpp
@@ -0,0 +1,22 @@
+#include "Global.h"
+#include "Plugin.h"
+
+ll::Logger logger(PLUGIN_NAME);
+
+namespace plugin {
+
+Plugin::Plugin(ll::plugin::NativePlugin& self) : mSelf(self) {
+    // Code for loading the plugin goes here.
+}
+
+bool Plugin::enable() {
+    // Code for enabling the plugin goes here.
+    return true;
+}
+
+bool Plugin::disable() {
+    // Code for disabling the plugin goes here.
+    return true;
+}
+
+} // namespace plugin
