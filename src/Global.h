b1598bcaa17aab3b2a997a87b2cc16574d2fb75e
子沐呀 <163636894+zimuya4153@users.noreply.github.com>
2024-12-01 00:18:14 +0800

    fix: temporarily fix the workflows build

--- src/Global.h
+++ src/Global.h
@@ -1,8 +1,10 @@
 #pragma once
 
+#include <span> // temporarily fix the workflows build
+
 #include "Entry.h"
 #include <include_all.h>
 
 #define MOD_NAME "GMLIB-Mod-Template"
-#define selfMod my_mod::Entry::getInstance()
-#define logger selfMod->getSelf().getLogger()
+#define selfMod  my_mod::Entry::getInstance()
+#define logger   selfMod->getSelf().getLogger()
